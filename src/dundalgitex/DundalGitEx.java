297a5fb6d37164d8b07971f7d56d6cc424ad6325
D00193120 <D00193120@P1104-PC03.ad.dkit.ie>
2017-01-26 10:11:38 +0000

    Jnevin method

--- src/dundalgitex/DundalGitEx.java
+++ src/dundalgitex/DundalGitEx.java
@@ -14,7 +14,10 @@ public class DundalGitEx {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+         System.out.println("Hello");
+        
         // TODO code application logic here
     }
     
